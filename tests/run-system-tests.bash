49970763cff88e1f6446083ff5872bce024b7ad9
Alberto Faria <albertofaria.dev@gmail.com>
2021-12-07 21:59:57 +0000

    Run system tests against a temporary k3d cluster
    Instead of relying on an existing Kubernetes cluster being accessible
    through the current kubectl context, use k3d to create a temporary one.

--- tests/run-system-tests.bash
+++ tests/run-system-tests.bash
@@ -3,58 +3,68 @@
 
 set -o errexit -o pipefail -o nounset
 
+script_dir="$( realpath -e "$0" | xargs dirname )"
+repo_root="$( realpath -e "${script_dir}/.." )"
+
 # ---------------------------------------------------------------------------- #
 # check usage
 
-build_and_push=1
-interactive=1
+fail_fast=0
+unset k3s_image
+wait_after_failure=0
+num_agents=0
 tests=()
 
-for arg in "$@"; do
-    case "${arg}" in
-        --no-build-and-push)
-            build_and_push=0
+while (( $# > 0 )); do
+    case "$1" in
+        --fail-fast)
+            fail_fast=1
             ;;
-        --no-interactive)
-            interactive=0
+        --k3s-image)
+            shift
+            k3s_image="$1"
+            ;;
+        --wait-after-failure)
+            wait_after_failure=1
+            ;;
+        --nodes)
+            shift
+            num_agents="$(( "$1" - 1 ))"
             ;;
         *)
-            if [[ -z "${image+set}" ]]; then
-                image="${arg}"
-            else
-                tests+=( "${arg}" )
-            fi
+            tests+=( "$1" )
             ;;
     esac
+    shift
 done
 
-image_regex='^([A-Za-z0-9._-]+:[0-9]+/)?([A-Za-z0-9._-]+/)*[A-Za-z0-9._-]+:[A-Za-z0-9._-]+$'
-
-{ [[ "${image:-}" =~ ${image_regex} ]] && (( ${#tests[@]} > 0 )); } || {
+if (( "${#tests[@]}" == 0 )); then
     >&2 echo -n "\
-Usage: $0 [<options...>] <registry>:<port>/<image>:<tag> <test...>
-       $0 [<options...>]                   <image>:<tag> <test...>
+Usage: $0 [<options...>] <tests...>
+       $0 [<options...>] all
 
-Run the given system tests against the active kubectl context.
+Run each given system test against a temporary k3d cluster.
 
-The first argument is the tag to use when building and pushing the PaV image.
-Note that <image> may contain '/' (slashes).
+If invoked with a single \`all\` argument, all .bash files under directory
+system-tests/ are run as tests.
 
-In the first form, the PaV image is built and pushed to the given repository
-before running the tests. Note that you must specify a repository port; this is
-to avoid pushing to public registries by mistake. In the second form, the PaV
-image is built but not pushed.
-
-In either form, adding flag --no-build-and-push will skip building and pushing
-the image before running the tests. Also, adding --no-interactive will skip all
-prompts.
+Options:
+   --fail-fast            Cancel remaining tests after a test fails.
+   --k3s-image <tag>      Use the given k3s image.
+   --nodes                Number of nodes in the cluster, including the control
+                          plane node, which can also run pods (default is 1).
+   --wait-after-failure   Prompt user for input after a test fails.
 "
     exit 2
-}
+fi
+
+if (( "${#tests[@]}" == 1 )) && [[ "${tests[0]}" = all ]]; then
+    tests=( "${script_dir}"/system-tests/*.bash )
+fi
 
 for test in "${tests[@]}"; do
     if [[ ! -e "${test}" ]]; then
-        >&2 echo "Test file does not exist: ${arg}"
+        >&2 echo "Test file does not exist: ${test}"
         exit 1
     fi
 done
@@ -62,22 +72,30 @@ done
 # ---------------------------------------------------------------------------- #
 # some private definitions
 
-script_dir="$( realpath -e "$0" | xargs dirname )"
-repo_root="$( realpath -e "${script_dir}/.." )"
-
 function _big_log()
 {
     local text term_cols sep_len
     text="$( printf "${@:2}" )"
     term_cols="$( tput cols 2> /dev/null )" || term_cols=80
     sep_len="$(( term_cols - ${#text} - 16 ))"
-    >&2 printf "\033[%sm--- [%s] %s " "$1" "$( date +%H:%M:%S )" "${text}"
-    printf '%*s\033[0m\n' "$(( sep_len < 0 ? 0 : sep_len ))" '' | >&2 tr ' ' -
+    printf "\033[%sm--- [%s] %s " "$1" "$( date +%H:%M:%S )" "${text}"
+    printf '%*s\033[0m\n' "$(( sep_len < 0 ? 0 : sep_len ))" '' | tr ' ' -
 }
 
-function _info()    { _big_log 33 "$@"; }
-function _success() { _big_log 32 "$@"; }
-function _error()   { _big_log 31 "$@"; }
+function _print_error()
+{
+    printf "\033[36m--- [%s] \033[31m%s\033[0m" \
+            "$( date +%H:%M:%S )" "$( printf "$@" )"
+}
+
+function _failure_prompt()
+{
+    _print_error "$1, use the following to point kubectl at the k3d cluster:"
+    echo
+    _print_error "   export KUBECONFIG=$KUBECONFIG"
+    echo
+    read -rp "$( _print_error "Press [ENTER] to clean up and continue... " )"
+}
 
 # ---------------------------------------------------------------------------- #
 # some definitions shared with the test scripts
@@ -89,7 +107,7 @@ function pav_log()
     (
         set -o errexit -o nounset +o xtrace
 
-        >&2 printf "\033[36m--- [%s] %s\033[0m\n" \
+        printf "\033[36m--- [%s] %s\033[0m\n" \
             "$( date +%H:%M:%S )" "$( printf "$@" )"
     )
 }
@@ -101,7 +119,7 @@ function pav_install()
         set -o errexit -o nounset +o xtrace
 
         pav_log "Installing PaV..."
-        sed "s|albertofaria/pav:[0-9+]\.[0-9+]\.[0-9+]|${image}|g" \
+        sed "s|albertofaria/pav:[0-9+]\.[0-9+]\.[0-9+]|pav:test|g" \
             "${repo_root}/deployment.yaml" | kubectl create -f -
 
         pav_log "Waiting for the controller agent to come up..."
@@ -117,27 +135,13 @@ function pav_uninstall()
         set -o errexit -o nounset +o xtrace
 
         pav_log "Uninstalling PaV..."
-        kubectl delete --ignore-not-found crd pavprovisioners.pav.albertofaria.github.io
-        kubectl delete --ignore-not-found -f "${repo_root}/deployment.yaml"
+        kubectl delete --ignore-not-found --timeout=60s \
+            crd pavprovisioners.pav.albertofaria.github.io
+        kubectl delete --ignore-not-found --timeout=60s \
+            -f "${repo_root}/deployment.yaml"
     )
 }
 export -f pav_uninstall
-
-function pav_wait_for_input()
-{
-    (
-        set -o errexit -o nounset +o xtrace
-
-        if (( interactive )); then
-            read -rp "$(
-                printf \
-                    "\033[33m[%s]\033[0m \033[33mPress [ENTER] to continue...\033[0m " \
-                    "$( date '+%H:%M:%S' )"
-                )"
-        fi
-    )
-}
-export -f pav_wait_for_input
 
 # Usage: pav_poll <retry_delay> <max_tries> <command>
 function pav_poll()
@@ -145,7 +149,7 @@ function pav_poll()
     (
         set -o errexit -o nounset +o xtrace
 
-        for (( i = 1; i < "$2"; ++i )); do
+        for (( __poll_index = 1; __poll_index < "$2"; ++__poll_index )); do
             if eval "${*:3}"; then return 0; fi
             sleep "$1"
         done
@@ -157,30 +161,74 @@ function pav_poll()
 }
 export -f pav_poll
 
-export -f pav_install
-export -f pav_uninstall
-export -f pav_wait_for_input
-export -f pav_poll
+# ---------------------------------------------------------------------------- #
+# build PaV image
+
+pav_log "Building PaV image (pav:test)..."
+docker image build --tag=pav:test "${repo_root}/image"
 
 # ---------------------------------------------------------------------------- #
-# build and push PaV image
+# create temporary directory
 
-if (( build_and_push )); then
+temp_dir="$( mktemp -d )"
+export KUBECONFIG="$temp_dir/kubeconfig"
 
-    pav_log "Building PaV image (%s)..." "${image}"
-    docker image build --tag="${image}" "${repo_root}/image"
+trap '{
+    k3d cluster delete pav-test
+    rm -fr "$temp_dir" || true
+    }' EXIT
 
-    if [[ "${image}" =~ ^[A-Za-z0-9._-]+:[0-9]+/ ]]; then
-        pav_log "Pushing PaV image (%s)..." "${image}"
-        docker image push "${image}"
-    fi
+function _create_k3d_cluster()
+{
+    local i volumes=()
 
-fi
+    pav_log 'Creating k3d cluster...'
+
+    # We need to mount a volume at /var/lib/kubernetes-pav with bidirectional
+    # mount propagation ("shared") for things to work.
+
+    mkdir "$temp_dir/vol-master"
+    volumes+=( "$temp_dir/vol-master:/var/lib/kubernetes-pav:shared@server:0" )
+
+    for (( i = 0; i < num_agents; ++i )); do
+        mkdir "$temp_dir/vol-$i"
+        volumes+=( "$temp_dir/vol-$i:/var/lib/kubernetes-pav:shared@agent:$i" )
+    done
+
+    # shellcheck disable=SC2068
+    k3d cluster create \
+        --agents "${num_agents}" \
+        ${k3s_image:+"--image=${k3s_image}"} \
+        --kubeconfig-switch-context=false \
+        --kubeconfig-update-default=false \
+        --no-lb \
+        ${volumes[@]/#/--volume } \
+        pav-test
+
+    k3d kubeconfig get pav-test > "$temp_dir/kubeconfig"
+}
+
+function _delete_k3d_cluster()
+{
+    local i
+
+    pav_log 'Deleting k3d cluster...'
+
+    k3d cluster delete pav-test
+
+    for (( i = 0; i < num_agents; ++i )); do
+        rm -fr "$temp_dir/vol-$i" || true
+    done
+
+    rm -fr "$temp_dir/vol-master" || true
+
+    rm -f "$temp_dir/kubeconfig"
+}
 
 # ---------------------------------------------------------------------------- #
 # run tests
 
-i=0
+test_i=0
 num_succeeded=0
 num_failed=0
 
@@ -192,15 +240,22 @@ for test in "${tests[@]}"; do
     test_abs="$( realpath -e "${test}" )"
     test_dir="$( dirname "${test_abs}" )"
 
-    _info 'Running test %s (%d of %d)...' "${test}" "$(( ++i ))" "${#tests[@]}"
+    _big_log 33 'Running test %s (%d of %d)...' \
+        "${test}" "$(( ++test_i ))" "${#tests[@]}"
+
+    _create_k3d_cluster
+
+    pav_log 'Importing PaV image into k3d cluster...'
+    k3d image import --cluster=pav-test --mode=direct pav:test
 
     set +o errexit
 
     (
-        set -o errexit -o pipefail -o nounset
-        cd "${test_dir}"
+        set -o errexit -o pipefail -o nounset -o xtrace
+
         pav_install
-        set -o xtrace
+
+        cd "${test_dir}"
         # shellcheck disable=SC1090
         source "${test_abs}"
     )
@@ -208,31 +263,39 @@ for test in "${tests[@]}"; do
     exit_code="$?"
     set -o errexit
 
-    if (( exit_code != 0 && interactive )); then
-        read -rp "$(
-            printf "\033[33m--- [%s] %s\033[0m " "$( date '+%H:%M:%S' )" \
-                'Test failed, press [ENTER] to clean up and continue...'
-            )"
+    if (( exit_code == 0 )); then
+
+        pav_uninstall || exit_code="$?"
+
+        if (( exit_code != 0 )); then
+            if (( wait_after_failure )); then
+                _failure_prompt 'Failed to uninstall PaV'
+            else
+                _print_error 'Failed to uninstall PaV.'
+                echo
+            fi
+        fi
+
+    else
+
+        if (( wait_after_failure )); then
+            _failure_prompt 'Test failed'
+        else
+            _print_error 'Test failed.'
+            echo
+        fi
+
     fi
 
-    (
-        set -o errexit -o pipefail -o nounset
+    _delete_k3d_cluster
 
-        pav_log 'Deleting objects created by test...'
-        kubectl delete deployment,job,pav,pod,pv,pvc,sc -l=pav-test-object
-
-        pav_uninstall || {
-            >&2 printf "\033[1;31mFailed to uninstall PaV.\033[0m\n"
-            exit 1
-        }
-    )
-
-    if (( canceled )); then break; fi
-
-    if (( exit_code == 0 )); then
+    if (( canceled )); then
+        break
+    elif (( exit_code == 0 )); then
         : $(( num_succeeded++ ))
     else
         : $(( num_failed++ ))
+        if (( fail_fast )); then break; fi
     fi
 
 done
@@ -252,6 +315,8 @@ fi
 
 _big_log "${color}" '%d succeeded, %d failed, %d canceled' \
     "${num_succeeded}" "${num_failed}" "${num_canceled}"
+
+trap 'rm -fr "$temp_dir" || true' EXIT
 
 (( num_succeeded == ${#tests[@]} ))
 
--- tests/system-tests/create-example-provisioners.bash
+++ tests/system-tests/create-example-provisioners.bash
@@ -4,10 +4,8 @@ for example in "${PAV_REPO_ROOT}/examples/"*; do
 
     pav_log "Create PavProvisioner from example ${example}..."
 
-    yq eval '.metadata.labels.pav-test-obj = ""' "${example}/provisioner.yaml" |
-        kubectl create -f -
-
-    kubectl delete -f "${example}/provisioner.yaml"
+    kubectl create -f "${example}/provisioner.yaml"
+    kubectl delete -f "${example}/provisioner.yaml" --timeout=60s
 
 done
 
